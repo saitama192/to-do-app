a2a117ac0ba489ae87b1bd070a5001d43cb904f5
kunal <walkolikunal@gmail.com>
2025-05-24 12:42:54 +0200

    added order in securityfilterchain

--- src/main/java/com/example/to_do_app/config/SecurityConfig.java
+++ src/main/java/com/example/to_do_app/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.to_do_app.config;
 import com.example.to_do_app.util.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
